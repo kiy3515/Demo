032a6b1e5d25f3d2d5c011aa31e93f08d927ef84
fxb <3121396345@qq.com>
2020-12-10 16:54:27 +0800

    上传姓名

---
+++ app/src/main/java/com/example/demogit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.demogit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String name = "冯晓斌";
+        String psd = "12345";
+    }
+}
