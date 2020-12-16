91a5069689bc96e18e6bbfb9d9771b204fcd8bfc
fxb <3121396345@qq.com>
2020-12-16 08:56:00 +0800

    账号添加

--- app/src/main/java/com/example/demogit/MainActivity.java
+++ app/src/main/java/com/example/demogit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String name = "冯晓斌";
+        String psw = "账号添加";
     }
 }
